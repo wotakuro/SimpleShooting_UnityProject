b901e983c40af0ffc434dfc5a99fc396d0a0accd
Yusuke Kurokawa <wotakuro@gmail.com>
2020-04-14 16:51:38 +0900

    update

--- Assets/Script/Game/StageTimeline.cs
+++ Assets/Script/Game/StageTimeline.cs
@@ -8,6 +8,7 @@ public class StageTimeline : MonoBehaviour
     void Start()
     {
         playableDirector = this.GetComponent<PlayableDirector>();
+        playableDirector.Stop();
         playableDirector.timeUpdateMode = DirectorUpdateMode.GameTime;
         playableDirector.Play();
         playableDirector.stopped += ChangeLevel;
